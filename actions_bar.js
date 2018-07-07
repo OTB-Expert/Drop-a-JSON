31da8a0013f44332119cc3f6a01d402a3e0a5522
Cumpanasu Florin <start_florin@yahoo.com>
2018-07-07 20:29:16 +0100

    Actions added

--- actions_bar.js
+++ actions_bar.js
@@ -3,14 +3,43 @@
 // ActionsBar will receive the data-id value to configure actions
 // Make syre you use jQuery
 
-function GenerateActionBar(data)
+function OtbGenerateActionBar(data)
 {
-  var actions_bar = $('<div><a class="fa-lg p-2 m-2 fb-ic"> <i class="fas fa-thumbtack"> </i> </a> <a class="fa-lg p-2 m-2 fb-ic"> <i class="fas fa-thumbs-up"> </i> </a> <a class="fa-lg p-2 m-2 fb-ic"> <i class="fas fa-copy"> </i> </a> <a class="fa-lg p-2 m-2 fb-ic"> <i class="fas fa-edit"> </i> </a> <a class="fa-lg p-2 m-2 fb-ic"> <i class="fas fa-share-alt"> </i> </a>
-            </a></div>');
+  var actions_bar = $('<div> <a class="fa-lg p-2 m-2 fb-ic"> <i class="fas fa-share-alt"> </i> </a> | <a class="fa-lg p-2 m-2 fb-ic"> <i class="fas fa-thumbtack"> </i> </a> <a class="fa-lg p-2 m-2 fb-ic"> <i class="fas fa-thumbs-up"> </i> </a> <a class="fa-lg p-2 m-2 fb-ic"> <i class="fas fa-copy"> </i> </a> <a class="fa-lg p-2 m-2 fb-ic"> <i class="fas fa-edit"> </i> </a> | <a class="fa-lg p-2 m-2 fb-ic"> <i class="fas fa-share-alt"> </i> </a></div>');
   return actions_bar;
+}
+
+function OtbActionShare(data)
+{
+  
+}
+
+function OtbActionPin(data)
+{
+  
+}
+
+function OtbActionLike(data)
+{
+  
+}
+
+function OtbActionEdit(data)
+{
+  
+}
+
+function OtbActionReport(data)
+{
+  
+}
+
+function OtbActionCopy(data)
+{
+  
 }
 
 // Display actions bar
 $(function () { 
-	$('.otb_actions_bar').append(GenerateActionBar({ "Id": 1 }));
+	$('.otb_actions_bar').append(OtbGenerateActionBar({ "Id": 1 }));
 })
