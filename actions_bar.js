65a42b83ba4693589ca3b6c98418ea62b1144104
Cumpanasu Florin <start_florin@yahoo.com>
2018-07-08 01:36:42 +0100

    Adding FB specific markups

--- actions_bar.js
+++ actions_bar.js
@@ -10,6 +10,11 @@ function OtbGenerateActionBar(data)
   return actions_bar;
 }
 
+function OtbInjectFacebook()
+{
+	
+}
+
 function OtbActionShare(data)
 {
   
@@ -41,6 +46,31 @@ function OtbActionCopy(data)
 }
 
 // Display actions bar
-$(function () { 
+$(function (){ 
+	$('body').append(`<head>
+  <title>Searching for Drops ?</title>
+  <meta property="og:url"           content="https://otb.expert/drops/" />
+  <meta property="og:type"          content="website" />
+  <meta property="og:title"         content="JSON Drops" />
+  <meta property="og:description"   content="Add drops to your code to transform JSON into beautifull UI" />
+  <meta property="og:image"         content="https://otb.expert/img/SmallJsonDrops.png" />
+</head>
+<body>
+
+  <!-- Load Facebook SDK for JavaScript -->
+  <div id="fb-root"></div>
+  <script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>
+
+  <!-- Your share button code -->
+  <div class="fb-share-button" 
+    data-href="https://www.your-domain.com/your-page.html" 
+    data-layout="button_count">
+  </div>`);
 	$('.otb_actions_bar').append(OtbGenerateActionBar({ "Id": 1 }));
 })
